86f0142bd3e629682cbffc739857fd1675c26c03
Arion <yngbhu@gmail.com>
2018-06-08 11:14:21 +0800

    init

---
+++ myprog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    int factorial = 1;
+    int number = 5;
+    
+    for (i = 1; i <= number; i++)
+    {
+        factorial = factorial * i;
+    }
+
+    printf("The factorial of %d is %d\n", number, factorial);
+    return 0;
+}
